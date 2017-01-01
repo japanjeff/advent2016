8b90c86d9fa44e8bb4e61a80284c137204884488
Jeffrey Bridgman <jeffrey.bridgman@gmail.com>
2017-01-01 14:36:04 -0600

    Function to activate a rectangle on the display

--- 8.py
+++ 8.py
@@ -7,6 +7,11 @@ def dump(display):
         print(''.join('#' if cell else '.' for cell in row))
     print()
 
+def activate(display, x, y):
+    for i in range(y):
+        for j in range(x):
+            display[i][j] = 1
+
 def rotate_column_down(display, column):
     temp = display[5][column]
     for row in range(5, 0, -1):
@@ -28,15 +33,5 @@ display[1][1] = 1
 display[2][2] = 1
 
 dump(display)
-rotate_row(display, 0, 3)
-dump(display)
-rotate_row(display, 0, -3)
-dump(display)
-rotate_column_up(display, 0)
-dump(display)
-rotate_column_up(display, 0)
-dump(display)
-rotate_column_down(display, 0)
-dump(display)
-rotate_column_down(display, 0)
+activate(display, 3, 2)
 dump(display)
