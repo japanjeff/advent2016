f83c67d1f83d2ac1f80b6ba7769d1b0d15943f7d
Jeffrey Bridgman <jeffrey.bridgman@gmail.com>
2017-01-01 14:21:02 -0600

    Get rotate_column to work... split into methods to rotate up and down

--- 8.py
+++ 8.py
@@ -7,10 +7,16 @@ def dump(display):
         print(''.join('#' if cell else '.' for cell in row))
     print()
 
-def rotate_column(display, column):
-    temp = display[0][column]
-    for row in range(5, 1):
+def rotate_column_down(display, column):
+    temp = display[5][column]
+    for row in range(5, 0, -1):
         display[row][column] = display[row-1][column]
+    display[0][column] = temp
+
+def rotate_column_up(display, column):
+    temp = display[0][column]
+    for row in range(0, 5):
+        display[row][column] = display[row+1][column]
     display[5][column] = temp
 
 def rotate_row(display, row, n):
@@ -25,4 +31,12 @@ dump(display)
 rotate_row(display, 0, 3)
 dump(display)
 rotate_row(display, 0, -3)
+dump(display)
+rotate_column_up(display, 0)
+dump(display)
+rotate_column_up(display, 0)
+dump(display)
+rotate_column_down(display, 0)
+dump(display)
+rotate_column_down(display, 0)
 dump(display)
