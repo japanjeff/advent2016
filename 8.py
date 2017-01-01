0c5499c5d58cb987c7af6cace885ad3e904507a8
Jeffrey Bridgman <jeffrey.bridgman@gmail.com>
2017-01-01 15:32:43 -0600

    Working code for day 8

--- 8.py
+++ 8.py
@@ -1,10 +1,10 @@
 columns = 50
 rows = 6
-display = [[None for x in range(columns)] for y in range(rows)]
+display = [[0 for x in range(columns)] for y in range(rows)]
 
 def dump(display):
     for row in display:
-        print(''.join('#' if cell else '.' for cell in row))
+        print(''.join('#' if cell else ' ' for cell in row))
     print()
 
 def activate(display, x, y):
@@ -13,25 +13,48 @@ def activate(display, x, y):
             display[i][j] = 1
 
 def rotate_column_down(display, column):
-    temp = display[5][column]
-    for row in range(5, 0, -1):
+    temp = display[rows-1][column]
+    for row in range(rows-1, 0, -1):
         display[row][column] = display[row-1][column]
     display[0][column] = temp
 
 def rotate_column_up(display, column):
     temp = display[0][column]
-    for row in range(0, 5):
+    for row in range(0, rows-1):
         display[row][column] = display[row+1][column]
-    display[5][column] = temp
+    display[rows-1][column] = temp
 
 def rotate_row(display, row, n):
     l = display[row]
     display[row] = l[-n:] + l[:-n]
 
-display[0][0] = 1
-display[1][1] = 1
-display[2][2] = 1
+# Test data
+# lines = """rect 3x2
+# rotate column x=1 by 1
+# rotate row y=0 by 4
+# rotate column x=1 by 1""".split('\n')
+
+with open('input-8.txt') as f:
+    lines = f.readlines()
+
+for line in lines:
+    task, _, parms = line.partition(' ')
+    if 'rect' in task:
+        x, y = parms.split('x')
+        activate(display, int(x), int(y))
+    elif 'rotate' in task:
+        target, num = parms.split('=')[-1].split(' by ')
+        if 'column' in parms:
+            for _ in range(int(num)):
+                rotate_column_down(display, int(target))
+        elif 'row' in parms:
+            rotate_row(display, int(target), int(num))
+        else:
+            assert False, 'Unknown rotate target'
+    else:
+        assert False, 'Unknown task'
 
 dump(display)
-activate(display, 3, 2)
-dump(display)
+
+pixels_on = sum(map(sum, display))
+print(pixels_on)
